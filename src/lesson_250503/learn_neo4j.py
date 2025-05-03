d17f46cd8bc1391cba1a0eb72b6ab2aa42101347
yeonw00 <mrk8056@gmail.com>
2025-05-03 16:56:20 +0900

    neo4j

---
+++ src/lesson_250503/learn_neo4j.py
@@ -0,0 +1,28 @@
+from neo4j import GraphDatabase
+
+driver = GraphDatabase.driver(
+    'bolt://127.0.0.1:7687', auth=('neo4j', 'dummypassword'))
+
+def clear_db(tx):
+    tx.run('MATCH (n) DETACH DELETE n')
+
+def add_friend(tx, name, friend_name=None):
+    if not friend_name:
+        return tx.run('CREATE (p:Person {name: $name}) RETURN p', name=name)
+    return tx.run('MATCH (p:Person {name: $name}) '
+                  'CREATE (p)-[:FRIEND]->(:Person {name: $friend_name})',
+                  name=name, friend_name=friend_name)
+
+
+def print_friend(tx, name):
+    for record in tx.run('MATCH (p:Person {name: $name})-[:FRIEND]->(f:Person)'
+                         'RETURN f.name', name=name):
+        print(record)
+
+with driver.session() as session:
+    session.execute_write(clear_db)
+    session.execute_write(add_friend, 'Jun')
+    for f in ['Mike', 'Nancy']:
+        session.execute_write(add_friend, 'Jun', f)
+    session.execute_read(print_friend, 'Jun')
+
